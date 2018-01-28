90ec48d469f40bce45c7d5c946a531a3a1a3cb80
zeerorg(Rishabh Gupta) <r.g.gupta@outlook.com>
2018-01-28 16:19:39 +0530

    Made steep hill climb to functional

--- algo/steep_hill_climbing.py
+++ algo/steep_hill_climbing.py
@@ -11,31 +11,18 @@ def steep_hill_climbing(problem: BaseProblem) -> bool:
     """
     Main function to start algo
     """
-    state = problem.start
-    visited = set()
-    while not problem.is_goal(state):
-        if state in visited:
-            return False
-
-        next_states = problem.get_child_states(state)
-        if compose(len, list, filter)(problem.is_goal, next_states) > 0:
-            return True
-
-        visited.add(state)
-
-        state = sorted(next_states, key=problem.get_heuristic)[-1]
-    return problem.is_goal(state)
+    return climb_state(problem, set(), problem.start)
 
 
 def climb_state(problem: BaseProblem, visited: set, state) -> bool:
     """
-    Steepest hill climbing
+    Steepest hill climbing node
     """
     next_states = sorted(problem.get_child_states(state), key=problem.get_heuristic)
     if state in visited:
         return False
 
-    if compose(len, filter)(problem.is_goal, next_states) > 0:
+    if compose(len, list, filter)(problem.is_goal, next_states) > 0:
         return True
 
-    return climb_state(problem, add_set(visited, state), next_states[0])
+    return climb_state(problem, add_set(visited, state), next_states[-1])
